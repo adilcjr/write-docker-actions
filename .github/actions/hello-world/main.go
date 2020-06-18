ef69743641a609a0d1fc51ff9241cd7b22bdc5d3
adilcjr <adil.calomeno.jr@gmail.com>
2020-06-18 15:08:13 -0300

    Update main.go

--- .github/actions/hello-world/main.go
+++ .github/actions/hello-world/main.go
@@ -1,12 +1,12 @@
 package main
 
-import {
-	"fmt"
-	"os"
-}
+import (
+  "fmt"
+  "os"
+)
 
 func main() {
-	// Access Inputs as environment vars
+  // Access Inputs as environment vars
   firstGreeting := os.Getenv("INPUT_FIRSTGREETING")
   secondGreeting := os.Getenv("INPUT_SECONDGREETING")
   thirdGreeting := os.Getenv("INPUT_THIRDGREETING")
